960db468dcfb11b859ed987c0ea5436ea62bcd39
Kyra Oja <k.oja@comcast.net>
2014-03-17 16:20:13 -0700

    base

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+// Make Foundation Go!
+$(document).foundation();
+
+// Your Awesome Scripts!
+$(document).ready(function(){
+
+	$.simpleWeather({
+    
+    location: '99203',
+    
+    success: function(weather) {
+      
+      // Get & Store Weather Data
+      // html = '<h2><i class="icon-' + weather.code+'"></i> ' + weather.temp +'&deg;' + weather.units.temp+'</h2>';
+      var temp, tomHi, tomLo, cityAndState; 
+
+      temp = weather.temp + '<span> f</span>';
+      tomHi = weather.tomorrow.high;
+      tomLo = weather.tomorrow.low;
+      cityAndState = weather.city + ' , ' + weather.region;
+
+      console.log(cityAndState);
+
+      // Display Weather
+      $('.temp').html(temp);
+      $('.tomHi').html(tomHi);
+      $('.tomLo').html(tomLo);
+      $('.cityAndState').html(cityAndState);
+
+    },
+    
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  
+  });
+
+	console.log('Page Loaded. Lets Do this!');
+
+}); 
